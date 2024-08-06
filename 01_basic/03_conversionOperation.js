8e91059ab5792de71fed8219ac7c336bad2f1b8b
Vishnu-raghav <vishnur1225@gmail.com>
2024-08-06 10:12:46 +0000

    conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "55abc";
+
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "vishnu" => true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
