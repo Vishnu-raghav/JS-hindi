ca6f2f482a700a7ec1d0a5e28dec59a27d3780da
Vishnu-raghav <vishnur1225@gmail.com>
2024-08-06 09:50:01 +0000

    Data Types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "vishnu";
+let age = 18;
+let isLoggedIn = false;
+
+// number
+// string
+// boolean => true/false
+// bigint
+// null
+// undefined
+// symbol
+
+
+// object
+
+console.log(typeof "vishnu");
