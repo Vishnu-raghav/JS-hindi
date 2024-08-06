e928cb5bb717f18f8e7675b3c3f6ec439c183971
Vishnu-raghav <vishnur1225@gmail.com>
2024-08-06 09:50:52 +0000

    Variables

---
+++ 01_basic/01_varibles.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let Email = "vishnuraghav.com";
+var paassword = "12345"
+accountcity = "faridabad"
+
+//  accountId = 2
+
+Email = 'vishnu@.com'
+paassword = '212121'
+accountcity = "delhi"
+
+console.table([accountId,paassword,Email,accountcity])
